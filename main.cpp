1950f3ce6109d4ae00b0ad941cc020b9b7ce2301
Francisco Silva <francisco.brito.silva@tecnico.ulisboa.pt>
2024-11-30 19:24:18 +0000

    alg

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 
 using namespace std;
 
-void readInput(ifstream& inputFile, int& n, int& m, vector<vector<int>>& table,
+void readInput(ifstream& inputFile, int& n, int& m, vector<vector<int>>& operationTable,
                vector<int>& sequence, int& result) {
     inputFile >> n >> m;
-    table.resize(n, vector<int>(n));
+    operationTable.resize(n, vector<int>(n));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            inputFile >> table[i][j];
+            inputFile >> operationTable[i][j];
         }
     }
     sequence.resize(m);  
@@ -24,10 +24,50 @@ int main(int argc, char* argv[]) {
     ifstream inputFile(argv[1]);
     int n, m, result;
     // ???? ver se o recise nao é mais lento
-    vector<vector<int>> table;
+    vector<vector<int>> operationTable;
     vector<int> sequence;
 
-    readInput(inputFile, n, m, table, sequence, result);
+    readInput(inputFile, n, m, operationTable, sequence, result);
+
+    // posso criar isto dentro do readInput para nao correr 2 vezes o mesmo for TODO
+    vector<vector<vector<int>>> dpTable(m, vector<vector<int>>(m)); 
+    for (int i = 0; i < m; i++) {
+        dpTable[i][i].push_back(sequence[i]);
+    }
+
+    for (int diagonal = 1; diagonal < m; diagonal++) {
+        for (int lin = 0; lin < m - diagonal; lin++) {
+            int col = lin + diagonal;
+            for (int i = diagonal - 1; i >= 0; i--) {
+                vector<bool> buffer(m, false); // buffer ocupa espaço mas é mais rapido
+                int solution_counter = 0;
+                for (int leftResult : dpTable[lin][lin + i]) {
+
+                    for (int rightResult : dpTable[lin + i + 1][col]) {
+                        int result = operationTable[leftResult - 1][rightResult - 1];
+                        if (solution_counter == m) {
+                            break;
+                        }
+                        if (buffer[result-1]) {
+                            continue;
+                        }
+                        dpTable[lin][col].push_back(result);
+                        int close_bracket = 0;
+                        dpTable[col][lin].push_back(close_bracket);
+                        dpTable[col][lin].push_back(leftResult);
+                        dpTable[col][lin].push_back(rightResult);
+                        buffer[result-1] = true;
+                        solution_counter++;
+                    }
+                    
+                    if (solution_counter == m) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
     inputFile.close();  
     return 0;
 }
